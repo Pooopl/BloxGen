872253f45af373492537d6071001a05ab376d966
Pooopl <124917017+Pooopl@users.noreply.github.com>
2023-06-11 13:38:10 +0100

    Update bloxgen.py
    Sory

--- bloxgen.py
+++ bloxgen.py
@@ -3,7 +3,7 @@ import random
 lower = "abcdefghijklmnopqrstuvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "1234567890"
-symbols = "_"
+symbols = @#£&_-()=%"*':/!?+,.$€¥¢©®™~¿[]{}<>^¡`;÷\|¦¬×§¶°
 
 string = lower + upper + numbers + symbols
 length = 12
